e643b499ea3eca7c4a744eebec3938e63b76a9b0
Leandro <leandromacedo12@gmail.com>
2013-11-15 04:51:11 -0200

    refiz a imagem do sol. Modelei como deve ser a arquetetura

--- home.php
+++ home.php
@@ -2,21 +2,28 @@
 <html lang="pt-BR">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Temperatura</title>
 </head>
 <body>
 
+<div id="corpo">
+	<div id="imagem">
+		<img src="img/sol.png">
+	</div>
+	<div id="temperatura">
 <?php
-require_once"pegainfo.class.php";
+	require_once"pegainfo.class.php";
 
-$temp=new Pegainfo;
-$graus=$temp->temperatura();
-
-echo $graus."ºC";
+	$temp=new Pegainfo;
+	$graus=$temp->temperatura();
+	echo $graus."ºC";
 
 ?>
+	</div>
+</div> <!--Fim div corpo--!>
 
-<img src="img/nublado.svg" />
+
 
 </body>
 </html>
